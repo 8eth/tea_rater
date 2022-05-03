5f8bee89ac7aa357efa2df2662007fd197a871b6
Betelihem <Betelihem>
2022-05-03 14:23:10 -0700

    updated routes for possible Heroku fix

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@
   post "/signup", to: "users#create"
   post "/login", to: "sessions#login"
   delete "/logout", to: "sessions#logout"
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   
 end
